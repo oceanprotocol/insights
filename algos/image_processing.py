7b137f225e9c1ab201094d2815b3cfa7c3ac9b91
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-08-30 15:26:25 +0300

    Added algos folder. Refactoring & fixing.

--- algos/image_processing.py
+++ algos/image_processing.py
@@ -2,21 +2,24 @@ from PIL import Image, ImageFilter
 import requests
 from io import BytesIO
 
-def apply_filters(image_url, filter, save_path='./output/'):
+def apply_filters(image_url, filter, save_path='output/'):
     response = requests.get(image_url)
     img = Image.open(BytesIO(response.content))
 
     # Apply filter
     if filter == 'blur':
         blurred_img = img.filter(ImageFilter.GaussianBlur(radius=5))
-        blurred_img.save(save_path + "_blurred.png")
+        blurred_img.save(save_path + "blurred.png")
     elif filter == 'grayscale':
         grayscale_img = img.convert("L")
-        grayscale_img.save(save_path + "_grayscale.png")
+        grayscale_img.save(save_path + "grayscale.png")
     else:
         print("Unknown filter.")
+    
+    return
 
 if __name__ == "__main__":
+    # The URL for image must be public and accessible
     image_url = input("Enter the URL of the image: ")
     img_filter = input("Enter the filter for the image: ")
 
