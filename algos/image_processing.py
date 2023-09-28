3f855374bf8381583db7eb71bc488503eeb37ba9
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-09-28 14:21:51 +0300

    tweak.

--- algos/image_processing.py
+++ algos/image_processing.py
@@ -40,5 +40,5 @@ if __name__ == "__main__":
     
     filtered_img = apply_filters(image_url, filter=img_filter)
     filename = "/data/outputs/filtered_image.png"
-    print(f"Filters applied and images saved successfully as {filename}")
     filtered_img.save(filename)
+    print(f"Filters applied and images saved successfully as {filename}")
