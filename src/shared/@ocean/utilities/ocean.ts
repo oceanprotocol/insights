aee0cc2135e71e4331471af6829466306a31a5af
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2023-11-06 17:34:07 +0200

    update multiplier

--- src/shared/@ocean/utilities/ocean.ts
+++ src/shared/@ocean/utilities/ocean.ts
@@ -1,17 +1,19 @@
-import { Config, ConfigHelper } from '@oceanprotocol/lib';
+import { Config, ConfigHelper } from "@oceanprotocol/lib";
 
 export function getOceanConfig(network: string | number): Config {
-  const config: Config = new ConfigHelper().getConfig(
-    network,
-    network === 'polygon' ||
-      network === 'moonbeamalpha' ||
-      network === 1287 ||
-      network === 'bsc' ||
-      network === 56 ||
-      network === 'gaiaxtestnet' ||
-      network === 2021000
-      ? undefined
-      : process.env.NEXT_PUBLIC_INFURA_PROJECT_ID,
-  ) as Config;
-  return config as Config;
+	const config: Config = new ConfigHelper().getConfig(
+		network,
+		network === "polygon" ||
+			network === "moonbeamalpha" ||
+			network === 1287 ||
+			network === "bsc" ||
+			network === 56 ||
+			network === "gaiaxtestnet" ||
+			network === 2021000
+			? undefined
+			: process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
+	) as Config;
+	config.gasFeeMultiplier =
+		config.chainId === 11155111 ? 1.6 : config.gasFeeMultiplier;
+	return config as Config;
 }
