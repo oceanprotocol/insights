7beaff0eb2d063ad893d6ff5681ca4d4a7a1f63b
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-11-06 12:59:14 +0200

    Modified env varname.

--- src/shared/utilities/wallet/index.ts
+++ src/shared/utilities/wallet/index.ts
@@ -36,13 +36,13 @@ export const wagmiClient = createConfig({
 						chainId: 80001,
 					},
 					{
-						rpcUrl: `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`,
+						rpcUrl: `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`,
 						chainId: 11155111,
 					},
 				],
 				magicSdkConfiguration: {
 					network: {
-						rpcUrl: `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`,
+						rpcUrl: `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`,
 						chainId: 11155111,
 					},
 					extensions: true,
